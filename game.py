6719727e20a486a6caec56f9cf89dfba761c7364
G7E9K <mr.evgenii.b@yandex.ru>
2023-10-11 17:24:17 +0300

    kovalev logic implemented

--- game.py
+++ game.py
@@ -14,7 +14,13 @@ def charPos(str, char):
     return [i for i, c in enumerate(str) if c == char]
 
 def handleKeySubmit(key, to_be_guessed, to_show, guessed_letters):
-    raise Exception("Не реализовано")
+    letter = key.lower()
+    if letter in to_be_guessed:
+        to_rotate = charPos(to_be_guessed, letter)
+        for i in to_rotate:
+            to_show = to_show[:i] + to_be_guessed[i] + to_show[i+1:]
+        guessed_letters += len(to_rotate)
+    return to_show, guessed_letters
 
 lib = None
 
@@ -36,7 +42,35 @@ def getUserInput(test, test_input=''):
         return (user_input, False)
 
 def startGame(test):
-    raise Exception("Не реализовано")
+    global lib
+    if not lib:
+        lib = init()
+    word = getRandomWord(lib)
+    to_be_guessed = word[0].lower()
+    to_show = "_" * len(word[0])
+    desc = word[1]
+    desc_to_show = desc[0]
+    desc = desc[1:]
+    guessed_letters = 0
+    attempts = 0
+    while not checkForWin(to_be_guessed, guessed_letters):
+        print(to_show)
+        print(desc_to_show)
+        print("Напишите букву или слово\n")
+        if test:
+            return
+        ans, is_letter = getUserInput(test)
+        if is_letter:
+            to_show, guessed_letters = handleKeySubmit(ans, to_be_guessed, to_show, guessed_letters)
+            attempts += 1
+            if attempts % 5 == 0 and len(desc):
+                desc_to_show += '\n' + desc[0]
+                desc = desc[1:]
+        else:
+            guessed_letters = handleWordSubmit(ans, to_be_guessed, guessed_letters)
+
+    print("ура победа")
+    startGame(False)
 
 if __name__ == '__main__':
     startGame(False)
